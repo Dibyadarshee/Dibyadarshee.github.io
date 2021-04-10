bf1a32860703fa64bb0707c478f695aa0bfa4a5a
Dibyadarshee <dibyadarsheepattnaik@gmail.com>
2021-04-10 15:50:48 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//selecting all required elements for gallery
+const filterItem = document.querySelector(".items");
+const filterImg = document.querySelector(".image");
+
+window.onload = () => {      //once window loaded
+  filterItem.onclick = (selectedItem) => { //when user clicked on filterItem div
+    if(selectedItem.target.classList.contains("item")){
+      filterItem.querySelector(".active").classList.remove(".active");   
+      selectedItem.target.classList.add("active"); // add that active class on the user selected element or item
+      let filterName = selectedItem.target.getAttribute("data-name"); //getting the data name value of the user selected item and storing in filtername variable 
+      console.log(filterName);
+    }
+  }
+}
